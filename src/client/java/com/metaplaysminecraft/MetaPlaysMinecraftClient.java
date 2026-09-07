75f9e3b5d42e9a005cc1fce9c1c64e57556acbad
Chayce22315 <chayceray@outlook.com>
2026-09-07 12:56:46 -0500

    wire ai controller into client tick

--- src/client/java/com/metaplaysminecraft/MetaPlaysMinecraftClient.java
+++ src/client/java/com/metaplaysminecraft/MetaPlaysMinecraftClient.java
@@ -1,10 +1,23 @@
 package com.metaplaysminecraft;
 
+import com.metaplaysminecraft.ai.AiPlayerController;
 import net.fabricmc.api.ClientModInitializer;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.minecraft.client.Minecraft;
 
 public final class MetaPlaysMinecraftClient implements ClientModInitializer {
-	@Override
-	public void onInitializeClient() {
-		MetaPlaysMinecraft.LOGGER.info("meta plays minecraft client initialized.");
-	}
+    private static AiPlayerController controller;
+
+    @Override
+    public void onInitializeClient() {
+        controller = new AiPlayerController();
+        ClientTickEvents.END_CLIENT_TICK.register(MetaPlaysMinecraftClient::onClientTick);
+        MetaPlaysMinecraft.LOGGER.info("meta plays minecraft ai controller initialized.");
+    }
+
+    private static void onClientTick(Minecraft minecraft) {
+        if (controller != null) {
+            controller.tick(minecraft);
+        }
+    }
 }
