cfc4f4f314067800143609435025c669b87a47e4
Chayce22315 <chayceray@outlook.com>
2026-09-07 14:08:56 -0500

    add f8 real bot join control

--- src/client/java/com/metaplaysminecraft/MetaPlaysMinecraftClient.java
+++ src/client/java/com/metaplaysminecraft/MetaPlaysMinecraftClient.java
@@ -1,13 +1,26 @@
 package com.metaplaysminecraft;
 
 import com.metaplaysminecraft.ai.AiPlayerController;
+import com.metaplaysminecraft.bridge.BotBridge;
 import com.metaplaysminecraft.perception.ChatMemory;
+import com.mojang.blaze3d.platform.InputConstants;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
+import net.fabricmc.fabric.api.client.keymapping.v1.KeyMappingHelper;
+import net.minecraft.client.KeyMapping;
 import net.minecraft.client.Minecraft;
+import net.minecraft.resources.Identifier;
 
 public final class MetaPlaysMinecraftClient implements ClientModInitializer {
     private static AiPlayerController controller;
+    private static final KeyMapping JOIN_BOT = KeyMappingHelper.registerKeyMapping(
+            new KeyMapping(
+                    "key.metaplaysminecraft.join_bot",
+                    InputConstants.Type.KEYSYM,
+                    InputConstants.KEY_F8,
+                    KeyMapping.Category.register(Identifier.fromNamespaceAndPath("metaplaysminecraft", "controls"))
+            )
+    );
 
     @Override
     public void onInitializeClient() {
@@ -18,6 +31,9 @@ public final class MetaPlaysMinecraftClient implements ClientModInitializer {
     }
 
     private static void onClientTick(Minecraft minecraft) {
+        while (JOIN_BOT.consumeClick()) {
+            BotBridge.toggle(minecraft);
+        }
         if (controller != null) {
             controller.tick(minecraft);
         }
